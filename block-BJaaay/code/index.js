f54c2d832317b23dc00e7cdeca16d8efd8a9969a
chirag gaur <chiragwalker@gmail.com>
2023-04-04 21:44:25 +0530

    all done

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -11,3 +11,50 @@
 // Calculate the sum of all numbers from 1 to 10.
 
 // Log all the values from 1 to 10 using while loop
+
+// 1.
+// for (let i = 1; i <=10; i++){
+//      console.log(i);
+// }
+
+// let i = 1 ;
+
+// while (i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// 2.
+
+// for (let i = 0 ; i<=20 ; i++){
+//       if( i % 2 === 0 ){
+//           console.log(i);
+//       }
+// }
+
+// for (let i = 0 ; i<=20 ; i++){
+//     if( i % 2 !== 0 ){
+//         console.log(i);
+//     }
+// }
+
+// let i = 1;
+// let total = 0;
+// while(i<=10){
+//     total = total+i;
+//     console.log(total);
+//     i++;
+// }
+
+// let total = 0;
+// for (i = 1; i<=10 ;i++){
+//     total = total + i ;
+//     console.log(total);
+// }
+
+// let i = 1;
+
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
