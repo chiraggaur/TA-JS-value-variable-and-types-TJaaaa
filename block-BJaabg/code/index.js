6795e8b490b9e69636693d24082bd506e76cf55f
chirag gaur <chiragwalker@gmail.com>
2023-04-05 16:53:15 +0530

    didn't got exact solution

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -12,7 +12,36 @@ const PHONE_PRICE = 99.99;
 const ACCESSORY_PRICE = 9.99;
 
 var bank_balance = 303.91;
-var amount = 0;
-// your code goes here
+var amount = 0 ;
+
+
+if  (bank_balance > amount) {
+    amount = PHONE_PRICE + ACCESSORY_PRICE + ((PHONE_PRICE + ACCESSORY_PRICE) * TAX_RATE );
+    bank_balance = bank_balance - amount ;
+    alert ("yes i can affort it");
+    // console.log(amount);
+
+} else {
+    alert ("i can't afford it");
+    
+}   
+
+
+    // console.log(amount);
+    // // limit_Remaining = SPENDING_THRESHOLD - amount ;
+    // bank_balance = (bank_balance - amount) ;
+    // amount += amount ; 
+     
+   
+    // console.log ("purchase amount is $" + amount );
+
+    // console.log (bank_balance);
 
 // ⛑ Answer of the above will `$334.76`.
+  // if (bank_balance > amount) {
+    //     alert ("You can but it ");
+    // } else {
+    //     alert (" Out of budget ");
+    // } 
+
+        // limit_Remaining  = (SPENDING_THRESHOLD - amount ); 
